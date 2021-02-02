5b278da274591d6f5e29c59b5536d4ae0d721280
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2021-02-02 11:03:49 +0100

    n/a

--- api-hooking.h
+++ api-hooking.h
@@ -1,5 +1,11 @@
 typedef char function_entry_point_bytes_t[6];
 
+struct {
+   void*                          fn_orig;
+   void*                          fn_hook;
+   function_entry_point_bytes_t   function_entry_point_bytes;
+} hook_t;
+
 void HookWinAPIFunction(
            void*       fn_orig,
            void*       fn_hook,
